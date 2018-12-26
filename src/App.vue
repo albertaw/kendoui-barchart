73985be71830ce37767d03c8d1c2de25f6bf6b31
Alberta Williams <alberta04@gmail.com>
2018-12-26 15:56:44 -0500

    create bar chart

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <kendo-datasource
+      ref="dataSource"
+      :transport-read-url="'https://api.iextrading.com/1.0/stock/amzn/chart'"
+      :transport-read-data-type="'json'">
+    </kendo-datasource>
+    <kendo-chart
+      :data-source-ref="'dataSource'"
+      :title-text="'Amazon Stock Prices'"
+      :legend-position="'bottom'"
+      :tooltip-visible="true"
+      :tooltip-template="'#: series.name #: $#: value #'"
+      :theme="'material'"
+      :series="series"
+      :category-axis="categoryAxis"
+      :value-axis="valueAxis">
+    </kendo-chart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      series: [{
+        type: 'column',
+        name: 'Close',
+        field: 'close'
+      }],
+      categoryAxis: {
+        field: 'label'
+      },
+      valueAxis: {
+        labels: {
+          format: '${0}'
+        }
+      }
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
+
+
